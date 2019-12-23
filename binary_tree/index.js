05a1c9b6d6c65ae03057dd45b9e18dc4e389855c
FredaFei <964959033@qq.com>
2019-12-23 14:52:59 +0800

    添加注释

--- binary_tree/index.js
+++ binary_tree/index.js
@@ -40,6 +40,14 @@ traverseRootLast(binaryTree, (value) => {
 })
 
 // 非递归解法
+/**
+ * 方法一
+ * 前序遍历
+ * 思路：
+ * 1.先遍历完左侧的所有节点并依次push to stack，执行fn
+ * 2.再依次出pop to stack，并检测右节点有无后代节点（回到53行代码）
+ * 3.stack 全部检测完退出循环，遍历完成
+ * */
 const traverseRootFirst = (bTree, fn) => {
   let current = bTree
   const stack = []
@@ -58,7 +66,14 @@ const traverseRootFirst = (bTree, fn) => {
     }
   }
 }
-
+/**
+ * 方法一
+ * 中序遍历
+ * 思路：
+ * 1.先遍历完左侧的所有节点并依次push to stack
+ * 2.再依次出pop to stack，执行fn，并检测右节点有无后代节点（回到53行代码）
+ * 3.stack 全部检测完退出循环，遍历完成
+ * */
 const traverseRootMiddle = (bTree, fn) => {
   let current = bTree
   const stack = []
@@ -77,6 +92,15 @@ const traverseRootMiddle = (bTree, fn) => {
     }
   }
 }
+/**
+ * 后序遍历
+ * 思路：
+ * 1.先将根节点push to stack
+ * 2.循环检测stack是否为空，若不为空，则pop stack，保存其值
+ * 3.检测其值左节点存在，存在则 push to stack
+ * 4.检测其值右节点存在，存在则 push to stack
+ * 5.执行fn （头部插入 unshift）
+ * */
 const traverseRootLast = (bTree, fn) => {
   if (!bTree) {return}
   let current = bTree
@@ -84,8 +108,8 @@ const traverseRootLast = (bTree, fn) => {
   stack.push(bTree)
   while (stack.length > 0) {
     current = stack.pop()
-    if(current.left){stack.push(current.left)}
-    if(current.right){stack.push(current.right)}
+    if (current.left) {stack.push(current.left)}
+    if (current.right) {stack.push(current.right)}
     fn(current.value)
   }
 }
@@ -94,3 +118,49 @@ const result4 = []
 traverseRootLast(binaryTree, (value) => {
   result4.unshift(value)
 })
+
+/**
+ * 方法二
+ * 前序遍历
+ * 思路：
+ * 1.先将根节点push to stack
+ * 2.循环检测stack是否为空，若不为空，则pop stack，保存其值
+ * 3.检测其值右节点存在，存在则 push to stack
+ * 4.检测其值左节点存在，存在则 push to stack
+ * 5.执行fn （尾部插入 push）
+ * */
+const traverseRootFirst = (bTree, fn) => {
+  if (!bTree) {return}
+  let current = bTree
+  const stack = []
+  stack.push(bTree)
+  while (stack.length > 0) {
+    current = stack.pop()
+    if (current.right) {stack.push(current.right)}
+    if (current.left) {stack.push(current.left)}
+    fn(current.value)
+  }
+}
+/**
+ * 方法二
+ * 中序遍历
+ * 思路：
+ * 1.先将根节点push to stack
+ * 2.循环检测stack或当前节点是否为空
+ * 3.若不为空，则push to stack，检测其左节点存在，存在则 push to stack，回到步骤2
+ * 4.若当前节点为空，则pop to stack，当前节点为其右节点，回到步骤2
+ * */
+const traverseRootMiddle = (bTree, fn) => {
+  let current = bTree
+  const stack = []
+  while (current || stack.length > 0) {
+    if (current) {
+      stack.push(current)
+      current = current.left
+    } else {
+      current = stack.pop()
+      fn(current.value)
+      current = current.right
+    }
+  }
+}
